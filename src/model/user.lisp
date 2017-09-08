00044b95885e4c70587d383dde442b51e7c5ec7d
makutak <kounotakuma@realglobe.jp>
2017-09-09 01:04:16 +0900

    パスワードの変更に対応
    スロットにアクセスできないのは、passwordはスロットではなく、総称関数なので。
    ちゃんとソース見よう。

--- src/model/user.lisp
+++ src/model/user.lisp
@@ -55,7 +55,7 @@
 (defun update-user (instance params)
   (setf (slot-value instance 'name) (get-value-from-params "name" params))
   (setf (slot-value instance 'email) (get-value-from-params "email" params))
-  ;;(setf (slot-value instance 'password) (get-value-from-params "password" params))
+  (setf (password instance) (get-value-from-params "password" params))
   (with-connection (db) (save-dao instance)))
 
 
